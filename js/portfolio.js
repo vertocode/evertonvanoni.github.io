b1f2987fb2082649bad34021e307c1a8a933ece9
Everton Vanoni <86416772+vertocode@users.noreply.github.com>
2021-08-13 17:05:01 -0300

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,6 +4,7 @@ let currentslide = 0;
 let slider = document.querySelectorAll('.slider--width')
 let item = document.querySelectorAll('.slider--item')
 
+//Avalia para saber se o slide vai voltar ou então avançar ao final.
 function goprev()
 {
     currentslide--;
@@ -31,6 +32,17 @@ function goprev3()
     }
     updateMargin3(); 
 }
+function goprev4()
+{
+    currentslide--;
+    if(currentslide < 0)
+    {
+        currentslide = totalslides - 1;
+    }
+    updateMargin4(); 
+}
+
+//Avalia para saber se o slide vai avançar ou então voltar ao inicio.
 function gonext()
 {
     currentslide++;
@@ -58,7 +70,17 @@ function gonext3()
     }
     updateMargin3();
 }
+function gonext4()
+{
+    currentslide++;
+    if(currentslide > (totalslides - 1))
+    {
+        currentslide = 0;
+    }
+    updateMargin4();
+}
 
+//Função onde faz a imagem avançar.
 function updateMargin()
 {
     let sliderwidth = document.querySelector('.slider--item').clientWidth;
@@ -75,7 +97,14 @@ function updateMargin3()
     let sliderwidth = document.querySelector('.tresitem').clientWidth;
    let newmargin3 = (currentslide * sliderwidth);    document.querySelector(".treswidth").style.marginLeft = `-${newmargin3}px`
 }
+function updateMargin4()
+{
+    let sliderwidth = document.querySelector('.quatroitem').clientWidth;
+   let newmargin4 = (currentslide * sliderwidth);    document.querySelector(".quatrowidth").style.marginLeft = `-${newmargin4}px`
+}
 
-setInterval(gonext, 5000)
-setInterval(gonext2, 5000)
-setInterval(gonext3, 5000)
+//Intervalos para avançar automaticamente.
+setInterval(gonext, 4000)
+setInterval(gonext2, 4000)
+setInterval(gonext3, 4000)
+setInterval(gonext4, 4000)
